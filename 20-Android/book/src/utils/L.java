3612170b9c70355ce431a710b9b6078b431a8e14
LexHsu <lexhsu@yeah.net>
2015-04-30 16:56:03 +0800

    Log

---
+++ 20-Android/book/src/utils/L.java
@@ -0,0 +1,47 @@
+package com.test.utils;
+
+import android.util.Log;
+
+/**
+ * Log 统一管理类
+ *
+ */
+public class L {
+
+    private L() {
+        /* cannot be instantiated */
+        throw new UnsupportedOperationException("cannot be instantiated");
+    }
+
+    public static boolean isDebug = true;
+
+    public static void v(String tag, String msg) {
+        if (isDebug) {
+            Log.v(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg) {
+        if (isDebug) {
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg) {
+        if (isDebug) {
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg) {
+        if (isDebug) {
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg) {
+        if (isDebug) {
+            Log.e(tag, msg);
+        }
+    }
+}

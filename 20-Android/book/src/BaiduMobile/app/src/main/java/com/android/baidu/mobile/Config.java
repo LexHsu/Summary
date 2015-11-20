97b97a5b0e05a08de92beecb9fec0b57344e9733
LexHsu <lexhsu@yeah.net>
2015-11-20 20:59:22 +0800

    clear

--- 20-Android/book/src/BaiduMobile/app/src/main/java/com/android/baidu/mobile/Config.java
+++
@@ -1,7 +0,0 @@
-package com.android.baidu.mobile;
-
-public class Config {
-    public static final String FIELD_KEYWORDS = "keywords";
-    public static final String SEARCH_URL = "http://www.baidu.com/s?wd=";
-
-}
